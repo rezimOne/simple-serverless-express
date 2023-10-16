ad868be0a4c05d294d4c131a3e9b42ca1a41b0dc
rezimOne <michalreda@gmail.com>
2023-10-17 00:45:29 +0200

    update

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 import express from 'express';
 const app = express();
 
-const port = 4000;
-app.use('/', (req, res, next) => {
-  res.send('Simple server on Vercel', req);
-});
-app.listen(port);
+const startServer = () => {
+  const port = 4000;
+  app.use('/', (req, res, next) => {
+    res.send('Simple server on Vercel', req);
+  });
+  app.listen(port);
+}
+startServer();
 
-module.exports(app);
+export default app;
 
